eb40338cbc211fb39ca11488580c8429d088cab8
HarshitChhipa <hchippa@expedien.in>
2018-09-16 22:07:52 +0530

    Pts nhi ksbks commit

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/electro-pair'));
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/electro-pair'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join('/dist/electro-pair/index.html'));
+app.get('*', function (req, res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/electro-pair/index.html'));
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
